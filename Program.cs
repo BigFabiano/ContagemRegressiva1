e3b36aad70a283aa83a5357edd0755ea1f845cd0
FabianoEtec <nbscbs@gmail.com>
2021-08-15 12:51:39 -0300

    contagem finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ContagemRegressiva1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar...");
+            Console.WriteLine("apontar...");
+            Console.WriteLine("fogo!");
+        }
+    }
+}
